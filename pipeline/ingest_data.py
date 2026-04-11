e531e57b6f0e5eb1e70d390944602e001ecc45df
M-Ibrahim-Ozdemir <muhammedibrahimozdemir10@gmail.com>
2026-04-11 11:01:44 +0000

    pipeline code working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,85 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
 
-year = 2021
-month = 1
-
-# Ayın başına otomatik 0 eklemek için :02d kullanıyoruz
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-
-df = pd.read_csv(url)
-
-
-# In[ ]:
-
-
-#karısık veri  tiplerine dair sutunlar  varmıs dıyor uayarı
-
-
-# In[5]:
-
-
-df.head()
-
-
-# In[ ]:
-
-
-#ne yaptık dockerda jupytter ayagara kaldırdık. NYC taxi bikırsmını çekip buu postgres içine koyacaz
-
-
-# In[6]:
-
-
-len(df)
-
-
-# In[7]:
-
-
-df
-
-
-# In[9]:
-
-
-df['VendorID']
-
-
-# In[ ]:
-
-
-#burda mesela 1.0 tamsayı ama NAN var float algılamıs. hataı veri algılamaıs MANDUS
-
-
-# In[10]:
-
-
-df['tpep_pickup_datetime']
-
-
-# In[ ]:
-
-
-#tarih tyğpe olmaslıo
-#aslında su sekilde pakette tur var, pandas turu taghmin etmeye calısıyor ama basaramıyor
-
-
-# In[ ]:
-
-
-
-
-
-# In[11]:
-
-
+# Veri tiplerini baştan tanımlamak bellek (RAM) dostudur
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -99,207 +22,46 @@ dtype = {
     "congestion_surcharge": "float64"
 }
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-df = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
-
-
-# In[13]:
-
-
-df.head()
-
-
-# In[14]:
-
-
-df['tpep_pickup_datetime']
-
-
-# In[ ]:
-
-
-#grdugmuz gibi datetime değişmiş dogru olmus...
-
-
-# In[ ]:
-
-
-##Şimdi sqlalchemy kutuphanesi:  python ve verşitabanları arası ilişki için
-
-
-# In[ ]:
-
-
-
-
-
-# In[21]:
-
-
-get_ipython().system('uv add sqlalchemy')
-
-
-
-
-
-#terminaldede yazsan aynı seyl olur
-
-
-
-
-
-get_ipython().system('uv add psycopg2-binary')
-
-
-
-
-pg_user = 'root'
-pg_pass = 'root'
-pg_host = 'localhost'
-pg_port = 5432
-pg_db = 'ny_taxi'
-
-from sqlalchemy import create_engine
-engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-
-
-# In[ ]:
-
-
-
-
-
-# In[26]:
-
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[ ]:
-
-
-#veritanaımız için olustrucak schema yı sağlıyor
-
-
-# In[28]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')   #amac su: yukardaki tabloyu olutruyo ama içine veri eklemiyor
-
-
-# terminalde
-# > cd ./pipeline/
-# > uv run pgcli -h localhost -p 5432 -u root -d ny_taxi
-# 
-# bunu deik verirabanı olsutrudıl
-
-# In[29]:
-
-
-len(df)
-
-
-# In[ ]:
-
-
-#bu cok buyuk parcalıyıp yuklemelizyiz. Yıneleyici kullnacaz
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[41]:
-
-
-df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=100000
-)
-
-
-# In[42]:
-
-
-#veri tablolaın her birini incelemek, parcaları veritabanına eklemek için
-get_ipython().system('uv add tqdm')
-
-
-# In[43]:
-
-
-from tqdm.auto import tqdm    #ilerlemeyi gormek icin. 
-
-
-# In[44]:
-
-
-for df_chunk in tqdm(df_iter):
-    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')  
-    #veritabanına ekler
-
-
-# vs cod terminalden
-# 
-# ls
-# README.md  pipeline  test
-# 
-# > cd ./pipeline/
-# > uv run pgcli -h localhost -p 5432 -u root -d ny_taxi
-# 
-# select COUNT(1) from yellow_taxi_data      konrtıo edersin gelmişmi vertabanına
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+
+def run():
+    pg_user, pg_pass = 'root', 'root'
+    pg_host, pg_port = 'localhost', 5432
+    pg_db = 'ny_taxi'
+    target_table = 'yellow_taxi_data'
+    
+    year, month = 2021, 1
+    chunksize = 100000
+    
+    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    # Iteratör oluşturuyoruz
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+
+    first = True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            # Önce tabloyu oluştur (hiç veri yüklemeden sadece şema)
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace', index=False)
+            first = False
+        
+        # Sonra veriyi ekle (append)
+        df_chunk.to_sql(name=target_table, con=engine, if_exists='append', index=False)
+
+    print("İşlem tamamlandı! 🚀")
+
+if __name__ == '__main__':
+    run()
+
+    """veriyi internetten (GitHub) çektik, parçalara böldüm (chunking) ve Docker üzerinde çalışan Postgres veri tabanına başarıyla akıttım."""
+    #uv run python ingest_data.py
+    #uv run pgcli -h localhost -p 5432 -u root -d ny_taxi
+    #\dt
+    #SELECT COUNT(1) FROM yellow_taxi_data;
