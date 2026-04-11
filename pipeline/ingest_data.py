ee09c7c6d2ec75527a747f784cc3e02ebc9174cf
M-Ibrahim-Ozdemir <muhammedibrahimozdemir10@gmail.com>
2026-04-11 14:01:16 +0000

    Docker network kuruldu ve pgAdmin hazırlandı

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 # Veri tiplerini baştan tanımlamak bellek (RAM) dostudur
 dtype = {
@@ -24,15 +25,17 @@ dtype = {
 
 parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
-def run():
-    pg_user, pg_pass = 'root', 'root'
-    pg_host, pg_port = 'localhost', 5432
-    pg_db = 'ny_taxi'
-    target_table = 'yellow_taxi_data'
-    
-    year, month = 2021, 1
-    chunksize = 100000
-    
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', type=int, default=5432, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', type=int, default=2021, help='Year for data')
+@click.option('--month', type=int, default=1, help='Month for data')
+@click.option('--chunksize', type=int, default=100000, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunksize):
     url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month:02d}.csv.gz'
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
@@ -65,3 +68,22 @@ if __name__ == '__main__':
     #uv run pgcli -h localhost -p 5432 -u root -d ny_taxi
     #\dt
     #SELECT COUNT(1) FROM yellow_taxi_data;
+
+
+
+
+
+    """use click to create cli paramets for this script               params are     pg_user, pg_pass = 'root', 'root'
+    pg_host, pg_port = 'localhost', 5432
+    pg_db = 'ny_taxi'
+    target_table = 'yellow_taxi_data'
+    
+    year, month = 2021, 1
+    chunksize = 100000
+    """
+
+#Capilota boyle soru sorduk...  komu satırı yapıyoruz
+
+
+
+    """Sobra Docker Image haline getirmeye başlayacaz. Yani "Bu kod sadece benim bilgisayarımda değil, dünyanın her yerinde tek komutla çalışsın" diyeceğiz."""
