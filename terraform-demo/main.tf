7c4490dc0f070ad8ce368e99a465eb06be8c14fb
M-Ibrahim-Ozdemir <muhammedibrahimozdemir10@gmail.com>
2026-04-18 12:04:05 +0000

    feat: infrastructure as code with variables and bigquery

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -8,15 +8,15 @@ terraform {
 }
 
 provider "google" {
-  project = "project-26ba53b4-6e64-4d2f-9a7"
-  region  = "us-central1"
+  project = var.project
+  region  = var.region
 }
 
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "project-26ba53b4-6e64-4d2f-9a7-terra-bucket"
-  location      = "US"
-  force_destroy = true
+  name                        = var.gcs_bucket_name
+  location                    = var.location
+  force_destroy               = true
   uniform_bucket_level_access = true
 
   lifecycle_rule {
@@ -38,5 +38,23 @@ resource "google_storage_bucket" "demo-bucket" {
   }
 }
 
-
 #bu kdolara tearraform cloud bucget vb yazarakdokımnakradan bulduk ıd falan ısım depiştiridk.İölerinde ne anlama geldiğine baabilirz
+
+#terrafrom biquery dataset diye aratarak ilgili siyteden adkdıl. 
+#cok değişiklik yaprık burda variable onur tarfat olustuduk entehre ettik
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
+#tekarar terraform apply dedik yes dedik bıquety de buldu. tabı once kaldırdıydık . Google cloude gittim bıgqueyde demo_dataset olostrdu. sonra terraform destroy kadlırdıyoz
+#sonra veriables.tf dosyaıs olututyorz
+#tekrardan bunun içeriğini guncelledikdetayıca her ıkı doysaıda sonra tekraradan terraform apply diyoruz terminale
+#bır sey daha yapcaz terraform destroy , tes dedilk
+
+#google ye unit variable linux arattırdık.  Ama biz yapmadık cunkuanahtar kullnamadık json falan
+# gcloud auth application-default print-access-token  terminaden bynu kontrol edelim
+#hersey yolunda terraform apply sonra yes de
+#tekrar terraform destroy yaptık gitti  hpesi
+
+#kod ile altyapi tanımlama, olustruma istediğimiz kaynakları soyeleyebilme bunları terrafom plam ile onzileme terraform apply ile dağıtma, defrrafrom destroy ile temizleme
+#Plan -> Apply -> Destroy
